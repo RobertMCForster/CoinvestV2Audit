7bcb50058216cb321a7643db9531a75dbd7a15e5
RobertMCForster <rforster510@yahoo.com>
2018-09-25 12:46:44 -0500

    Edited to Include V1 -> V3 Exchange

--- PostAuditFinals/test/TokenSwap.js
+++ PostAuditFinals/test/TokenSwap.js
@@ -23,6 +23,7 @@ const should = require('chai')
 // Contract constants
 const TokenSwap         = artifacts.require('../contracts/TokenSwap.sol')
 const Token				= artifacts.require('../contracts/CoinvestToken.sol')
+const Token223    = artifacts.require('../contracts/Coinvest223.sol')
 
 // Promisify get balance of ether
 const promisify = (inner) =>
@@ -42,47 +43,83 @@ const getBalance = (account, at) =>
   
       this.owner             = web3.eth.accounts[0];
   
-      this.oldToken          = await Token.new({from:this.owner});
-      this.newToken          = await Token.new({from:this.owner});
-      this.tokenSwap         = await TokenSwap.new(this.oldToken.address, this.newToken.address, {from:this.owner});
-      this.web3              = this.oldToken.web3;
+      this.tokenV1          = await Token223.new(0,{from:this.owner});
+      this.tokenV2          = await Token.new({from:this.owner});
+      this.tokenV3          = await Token.new({from:this.owner});
+      this.tokenSwap        = await TokenSwap.new(this.tokenV1.address, this.tokenV2.address, this.tokenV3.address, {from:this.owner});
   
       // Put all new tokens into the token swap contract.
-      let balance = await this.newToken.balanceOf(this.owner)
-      await this.newToken.transfer(this.tokenSwap.address, balance, {from:this.owner})
+      let balance = await this.tokenV3.balanceOf(this.owner)
+      await this.tokenV3.transfer(this.tokenSwap.address, balance, {from:this.owner})
 
     })
   
   /** ********************************** Core ***************************************** */
   
   
-    describe('All TokenSwap checks', function () {
-      
-        it('should have correct addresses on construction', async function () {
-            let oldAddress = await this.tokenSwap.oldToken.call()
-            let newAddress = await this.tokenSwap.newToken.call()
-            oldAddress.should.be.equal(this.oldToken.address)
-            newAddress.should.be.equal(this.newToken.address)
-        })
+    describe('V2 -> V3 Swap', function () {
 
         it('should exchange correct amount', async function () {
-            let initialBalance = await this.oldToken.balanceOf(this.owner)
-            await this.oldToken.approveAndCall(this.tokenSwap.address, initialBalance, '')
+            let initialBalance = await this.tokenV2.balanceOf(this.owner)
+            await this.tokenV2.approveAndCall(this.tokenSwap.address, initialBalance, '')
 
-            let oldTokenBal = await this.oldToken.balanceOf(this.owner)
+            let oldTokenBal = await this.tokenV2.balanceOf(this.owner)
             oldTokenBal.should.be.bignumber.equal(0)
 
-            let newTokenBal = await this.newToken.balanceOf(this.owner)
+            let newTokenBal = await this.tokenV3.balanceOf(this.owner)
             newTokenBal.should.be.bignumber.equal(initialBalance)
 
-            let newSwapBal = await this.newToken.balanceOf(this.tokenSwap.address)
+            let newSwapBal = await this.tokenV3.balanceOf(this.tokenSwap.address)
             newSwapBal.should.be.bignumber.equal(0)
         })
 
-        it('should fail if token is not caller', async function () {
-            await this.tokenSwap.receiveApproval(this.owner,100,this.oldToken.address,'').should.be.rejectedWith(EVMRevert)
+        it('should fail if tokenV2 is not caller', async function () {
+            await this.tokenSwap.receiveApproval(this.owner,100,this.tokenV2.address,'', {from:this.owner}).should.be.rejectedWith(EVMRevert)
+        })
+
+        it('should fail if amount is 0', async function () {
+          await this.tokenV2.approveAndCall(this.tokenSwap.address, 0, '').should.be.rejectedWith(EVMRevert)
         })
 
     })
 
-  })
+    describe('V1 -> V3 Swap', function () {
+
+      it('should exchange correct amount', async function () {
+          let initialBalance = await this.tokenV1.balanceOf(this.owner)
+          await this.tokenV1.transfer(this.tokenSwap.address, initialBalance)
+
+          let v1bal = await this.tokenV1.balanceOf(this.owner)
+          v1bal.should.be.bignumber.equal(0)
+
+          let v3bal = await this.tokenV3.balanceOf(this.owner)
+          v3bal.should.be.bignumber.equal(initialBalance)
+
+          let newSwapBal = await this.tokenV3.balanceOf(this.tokenSwap.address)
+          newSwapBal.should.be.bignumber.equal(0)
+      })
+
+      it('should fail if token is not caller', async function () {
+          await this.tokenSwap.tokenFallback(this.owner,100,'').should.be.rejectedWith(EVMRevert)
+      })
+
+      it('should fail if amount is 0', async function () {
+        await this.tokenV1.transfer(this.tokenSwap.address, 0).should.be.rejectedWith(EVMRevert)
+      })
+
+    })
+
+    describe('Constants', function () {
+
+      it('should have correct addresses on construction', async function () {
+        let tokenV1 = await this.tokenSwap.tokenV1.call()
+        let tokenV2 = await this.tokenSwap.tokenV2.call()
+        let tokenV3 = await this.tokenSwap.tokenV3.call()
+        tokenV1.should.be.equal(this.tokenV1.address)
+        tokenV2.should.be.equal(this.tokenV2.address)
+        tokenV3.should.be.equal(this.tokenV3.address)
+      })
+
+    })
+
+})
