c4b2c9385877dce75d311b6307d3f8f00b412c5f
RobertMCForster <rforster510@yahoo.com>
2018-09-25 12:45:39 -0500

    Edited to Include V1 -> V3 Exchange

--- PostAuditFinals/contracts/TokenSwap.sol
+++ PostAuditFinals/contracts/TokenSwap.sol
@@ -1,36 +1,44 @@
-pragma solidity ^0.4.24;
-import './CoinvestToken.sol';
-
-contract TokenSwap {
+pragma solidity ^0.4.24; import './ERC20Interface.sol'; contract TokenSwap {
     
-    CoinvestToken public oldToken;
-    CoinvestToken public newToken;
+    ERC20Interface public tokenV1;
+    ERC20Interface public tokenV2;
+    ERC20Interface public tokenV3;
     
     /**
-     * @param _oldToken Address of old COIN token.
-     * @param _newToken Address of new COIN token.
     **/
-    constructor(address _oldToken, address _newToken) 
-      public
-    {
-        oldToken = CoinvestToken(_oldToken);
-        newToken = CoinvestToken(_newToken);
+    constructor(address _tokenV1, address _tokenV2, address _tokenV3) {
+        tokenV1 = ERC20Interface(_tokenV1);
+        tokenV2 = ERC20Interface(_tokenV2);
+        tokenV3 = ERC20Interface(_tokenV3);
     }
-
+    /**
+     * @param _from The address that has transferred this contract tokens.
+     * @param _value The amount of tokens that have been transferred.
+     * @param _data The extra data sent with transfer (should be nothing).
+    **/
+    function tokenFallback(address _from, uint _value, bytes _data)
+      external
+    {
+        require(msg.sender == address(tokenV1));
+        require(_value > 0);
+        require(tokenV3.transfer(_from, _value));
+        _data;
+    }
     /**
      * @dev approveAndCall will be used on the old token to transfer from the user
-     *      to the contract, which will then return to them the new tokens.
+     * to the contract, which will then return to them the new tokens.
      * @param _from The user that is making the call.
      * @param _amount The amount of tokens being transferred to this swap contract.
      * @param _token The address of the token contract (address(oldToken))--not used.
      * @param _data Extra data with the call--not used.
     **/
-    function receiveApproval(address _from, uint256 _amount, address _token, bytes _data) 
+    function receiveApproval(address _from, uint256 _amount, address _token, bytes _data)
       public
     {
-        require(msg.sender == address(oldToken));
-        require(oldToken.transferFrom(_from, address(this), _amount));
-        require(newToken.transfer(_from, _amount));
+        require(msg.sender == address(tokenV2));
+        require(_amount > 0);
+        require(tokenV2.transferFrom(_from, address(this), _amount));
+        require(tokenV3.transfer(_from, _amount));
         _token; _data;
     }
     
