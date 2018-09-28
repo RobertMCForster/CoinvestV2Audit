aa9409c12f028fb8ab686fbf5e5da186f09e011b
RobertMCForster <rforster510@yahoo.com>
2018-09-28 11:27:54 -0500

    Added tokenEscape

--- PostAuditFinals/contracts/TokenSwap.sol
+++ PostAuditFinals/contracts/TokenSwap.sol
@@ -1,10 +1,17 @@
-pragma solidity ^0.4.24; import './ERC20Interface.sol'; contract TokenSwap {
+pragma solidity ^0.4.24; 
+import './ERC20Interface.sol'; 
+import './Ownable.sol';
+
+contract TokenSwap is Ownable {
     
     ERC20Interface public tokenV1;
     ERC20Interface public tokenV2;
     ERC20Interface public tokenV3;
     
     /**
+     * @param _tokenV1 The original ERC223 version of the Coinvest token.
+     * @param _tokenV2 The second iteration of the token using ERC865.
+     * @param _tokenV3 The new iteration of the Coinvest token.
     **/
     constructor(address _tokenV1, address _tokenV2, address _tokenV3) public {
         tokenV1 = ERC20Interface(_tokenV1);
@@ -42,4 +49,25 @@ pragma solidity ^0.4.24; import './ERC20Interface.sol'; contract TokenSwap {
         _token; _data;
     }
     
+    /**
+     * @dev Allow the owner to take Ether or tokens off of this contract if they are accidentally sent.
+     * @param _tokenContract The address of the token to withdraw (0x0 if Ether).
+     * @notice This allows Coinvest to take all valuable tokens from the TokenSwap contract.
+    **/
+    function tokenEscape(address _tokenContract)
+      external
+      onlyCoinvest
+    {
+        // Somewhat pointless require as Coinvest can withdraw V2 and exchange for more V3.
+        require(_tokenContract != address(tokenV1) && _tokenContract != address(tokenV3));
+        
+        if (_tokenContract == address(0)) coinvest.transfer(address(this).balance);
+        else {
+            ERC20Interface lostToken = ERC20Interface(_tokenContract);
+        
+            uint256 stuckTokens = lostToken.balanceOf(address(this));
+            lostToken.transfer(coinvest, stuckTokens);
+        }    
+    }
+
 }
